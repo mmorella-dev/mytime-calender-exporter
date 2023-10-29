c2ef3458329a425bc0d243aad7c1c9aa43ac2521
Mae Morella <4561733+morellam-dev@users.noreply.github.com>
2023-10-29 13:19:11 -0400

    Update (added type comments)

--- mytime_parser.user.js
+++ mytime_parser.user.js
@@ -4,114 +4,164 @@
 // @namespace   Violentmonkey Scripts
 // @match       https://mytime.target.com/*
 // @grant       GM_registerMenuCommand
-// @version     1.0
+// @version     1.1
 // @author      -
 // @require https://cdn.jsdelivr.net/npm/@violentmonkey/dom@2
 // @require https://raw.githubusercontent.com/nwcell/ics.js/dfec67f37a3c267b3f97dd229c9b6a3521222794/ics.deps.min.js
 // ==/UserScript==
 
+/** @typedef {{ job: string, location: string, startDate: Date, endDate: Date, naiveDate: string }} Shift */
+/** @typedef {[year: number, monthIndex: number, day: number ]} DateComponents */
+/** @typedef {[hour: number, minute: number ]} TimeComponents */
+
+/** The expression used to parse shift labels. This is gonna break if Target modifies the frontend. */
+const SHIFT_LABEL_REGEX =
+  /^(.+) shift from (.+) to (.+) at location (.+) on (.+)\. Click to view daily view/;
+
+/** @type {Map<string, Shift>} */
 const allShifts = new Map();
 
-// scan the current week of shifts.
+/** Populates allShifts with values from the DOM */
 const scanSchedule = () => {
   for (let i = 0; i < 7; i++) {
-    const dayEl = document.getElementById(`${i}`); // there's a numeric ID for each day of the week. weird but helpful.
-    if (!dayEl) {
+    // <div id="0" aria-label="schedule for 2023-12-31 with 1 shifts">
+    const scheduleEl = document.getElementById(`${i}`);
+    // there's a numeric ID for each day of the week. weird but helpful.
+    if (!scheduleEl) {
       break;
     }
-    const dayLabel = dayEl.getAttribute("aria-label");
-    // scan for children with shifts
-    const shiftEls = dayEl.querySelectorAll(`a[aria-label*="shift from"]`);
+    const dayLabel = scheduleEl.getAttribute("aria-label");
+    // <a aria-label="Tech shift from 05:00PM to 10:00PM on Monday October 12. Click to view daily view">
+    const shiftEls = scheduleEl.querySelectorAll(`a[aria-label*="shift from"]`);
     for (const shiftEl of shiftEls) {
       const shiftLabel = shiftEl.getAttribute("aria-label");
-      if (!allShifts.has(shiftLabel)) {
+      if (shiftLabel && !allShifts.has(shiftLabel)) {
+        // @ts-ignore
         const shift = parseShift(dayLabel, shiftLabel);
         allShifts.set(shiftLabel, shift);
       }
     }
   }
+  console.debug(allShifts);
 };
 
-const parseShift = (dayLabel, shiftLabel) => {
-  const date = parseDayLabel(dayLabel);
-  const { job, start, end, naiveDate, location } = parseShiftLabel(shiftLabel);
-  const startDate = formatDateTime(date, start);
-  const endDate = formatDateTime(date, end);
-  return { job, location, startDate, endDate, naiveDate };
-}
-
-// first param is the result of
-// date: [y, m, d];
-// time: "05:00PM"
-const formatDateTime = (date, time) => {
-  const [hours, mins] = parseTime(time);
-  const [year, month, day] = date;
-  return new Date(year, month, day, hours, mins);
-}
-
-const parseDayLabel = (label) => {
-  // takes the aria-label from a given day, and parses it into components for the Date constructor
-  const LABEL_REGEX = /^schedule for (?<year>\d{4})-(?<month>\d{2})-(?<day>\d{2})/;
-  const { year, month, day } = LABEL_REGEX.exec(label)?.groups; // YYYY-MM-DD
-  return [
-    parseInt(year),
-    parseInt(month) - 1, // months are 0-indexed
-    parseInt(day)
-  ];
-}
-
-const parseTime = (time) => {
-  var parts = time.split(":");
-  var hour = parseInt(parts[0]);
-  var minute = parseInt(parts[1]);
-  // Adjust the hour if it's in the afternoon (PM)
-  if (parts[1].includes("PM") && hour < 12) {
-    hour += 12;
+/**
+ * @param {string} scheduleLabel aria-label
+ * @param {string} shiftLabel aria-label
+ * @returns {Shift} */
+const parseShift = (scheduleLabel, shiftLabel) => {
+  const date = parseDate(scheduleLabel);
+  const { job, startTime, endTime, naiveDate, location } =
+    parseShiftLabel(shiftLabel);
+  const startDate = new Date(...date, ...startTime);
+  const endDate = new Date(...date, ...endTime);
+  if (startDate.toString() === "Invalid Date" || endDate.toString() === "Invalid Date") {
+    console.error([...date, ...startTime]);
+    console.error([...date, ...endTime]);
+    throw new Error(`Error parsing dates`);
   }
-  return [hour, minute];
-}
+  return { job, location, startDate, endDate, naiveDate };
+};
 
+/**
+ * @param {string} label string containing a date YYYY-MM-DD
+ * @returns {DateComponents} [y, m, d] */
+const parseDate = (label) => {
+  // takes the aria-label from a given day, and parses it into components for the Date constructor
+  const matches = label.match(/(\d{4})-(\d{2})-(\d{2})/);
+  if (matches?.length !== 4) {
+    throw new Error(`Could not parse date from ${label}`);
+  }
+  const year = parseInt(matches[1]);
+  const month = parseInt(matches[2]) - 1;
+  const day = parseInt(matches[3]);
+  return [year, month, day];
+};
+
+/**
+ * @param {string} time String of the form "12:34PM"
+ * @returns {TimeComponents} 24 hour time components */
+const parseTime = (time) => {
+  const parts = time.match(/(\d{2}):(\d{2})(AM|PM)/);
+  if (parts?.length !== 4) {
+    throw new Error(`Could not parse ${time} as time.`);
+  }
+  const hour = parseInt(parts[1]);
+  const minute = parseInt(parts[2]);
+  const isPM = parts[3] === "PM";
+  // convert to 24hrs
+  const hour24 = isPM && hour < 12 ? hour + 12 : hour;
+  return [hour24, minute];
+};
+
+/**
+ * @param {string} label aria-label value of the form "Tech shift from 05:00PM to 10:00PM on Monday October 12. Click to view daily view"
+ * @returns {{ job: string, location: string, startTime: TimeComponents, endTime: TimeComponents, naiveDate: string }}
+ */
 const parseShiftLabel = (label) => {
-  // This is gonna break if Target modifies the frontend. Should probably build a more lenient parser.
-  // (JOB) shift from (HH:MMAM) to (HH:MMPM) to
-  const LABEL_REGEX = /^(?<job>\w+) shift from (?<start>\d{2}:\d{2}.{2}) to (?<end>\d{2}:\d{2}.{2}) at location (?<location>\d+?) on (?<naiveDate>.*)\. Click to view daily view/;
-  return LABEL_REGEX.exec(label)?.groups;
-}
+  const matches = label.match(SHIFT_LABEL_REGEX);
+  if (matches?.length !== 6) {
+    console.error(matches);
+    throw new Error(`Couldn't parse shift info from \`${label}\``);
+  }
+  const [, job, start, end, location, naiveDate ] = matches;
+  const startTime = parseTime(start);
+  const endTime = parseTime(end);
+  return { job, location, startTime, endTime, naiveDate };
+};
 
 let currentDay0 = document.getElementById("0");
 let currentCount = allShifts.size;
 
-// Watch for changes
-const disconnect = VM.observe(document.querySelector('#root'), () => {
+// Watch the DOM for changes
+// @ts-ignore
+const disconnect = VM.observe(document.querySelector("#root"), () => {
   // check that the week has been replaced
   if (currentDay0 != document.getElementById("0")) {
     currentDay0 = document.getElementById("0");
-    scanSchedule();
+    try {
+      scanSchedule();
+    } catch (e) {
+      alert(e);
+    }
   }
 });
 
+/** @param {Iterable<Shift>} shifts */
 const makeICSFile = (shifts) => {
-  if (!shifts || shifts.length == 0) {
+  if (!shifts) {
     window.alert("No shifts found for this week. Maybe a parse error?");
     return;
   }
+  // @ts-ignore
   const cal = ics();
   let lastDate;
   for (const s of shifts) {
     lastDate = s.naiveDate;
-    cal.addEvent(`${s.job} shift`, ``, `Target #${s.location}`, s.startDate, s.endDate);
+    cal.addEvent(
+      `${s.job} shift`,
+      ``,
+      `Target #${s.location}`,
+      s.startDate,
+      s.endDate
+    );
   }
   cal.download(`Target Shifts as of ${lastDate}`);
   // alert("Not implemented yet!");
-}
+};
 
 const promptICSFile = () => {
-    if (!window.confirm(`Download ICS file for ${allShifts.size} shifts?`)) {
-      return;
-    }
+  if (!window.confirm(`Download ICS file for ${allShifts.size} shifts?`)) {
+    return;
+  }
+  try {
     makeICSFile(allShifts.values());
-}
+  } catch (e) {
+    alert(e);
+  }
+};
 
-const cmdID = GM_registerMenuCommand("Export ICS file", promptICSFile);
+// @ts-ignore
+GM_registerMenuCommand("Export ICS file", promptICSFile);
 
 scanSchedule();
